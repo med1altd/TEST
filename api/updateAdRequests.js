c50b3d0f223d88ceaac7bdc329819fcb594be072
med1altd <139887894+med1altd@users.noreply.github.com>
2024-02-26 02:57:51 +0200

    Update updateAdRequests.js

--- api/updateAdRequests.js
+++ api/updateAdRequests.js
@@ -42,7 +42,7 @@ module.exports = async (req, res) => {
     const year = currentDate.getFullYear();
     const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Adding 1 because getMonth() returns zero-based month
     const day = String(currentDate.getDate()).padStart(2, '0');
-    const hours = String(currentDate.getHours()).padStart(2, '0');
+    const hours = String(currentDate.getHours()).padStart(2, '0') + 2;
     const minutes = String(currentDate.getMinutes()).padStart(2, '0');
     const seconds = String(currentDate.getSeconds()).padStart(2, '0');
     
