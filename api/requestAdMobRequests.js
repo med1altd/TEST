1178d2d55eba257916e042a0ed59dfa9620b9f18
med1altd <139887894+med1altd@users.noreply.github.com>
2024-02-26 17:49:33 +0200

    Update requestAdMobRequests.js

--- api/requestAdMobRequests.js
+++ api/requestAdMobRequests.js
@@ -40,10 +40,10 @@ module.exports = async (req, res) => {
     const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Adding 1 because getMonth() returns zero-based month
     const day = String(currentDate.getDate()).padStart(2, '0');
     const hours = String((currentDate.getHours() + 2) % 24).padStart(2, '0');
-    const minutes = String(currentDate.getMinutes()).padStart(2, '0');
-    const seconds = String(currentDate.getSeconds()).padStart(2, '0');
-    
-    const now = `${year}-${month}-${day}-${hours}`;
+
+    const now = `${year}-${month}-${day}`;
+
+    const hour = `${hours}:00`;
     
     // Query the Google Sheet to find today's date
     
@@ -60,7 +60,7 @@ module.exports = async (req, res) => {
     
     if (values) {
       
-      todayIndex = values.findIndex((row) => row[0] === now);
+      todayIndex = values.findIndex(row => row[0] === now && row[1] === hour);
     
     }
 
@@ -71,7 +71,6 @@ module.exports = async (req, res) => {
     
       let rowData = values[todayIndex];
 
-    
       // Ensure that rowData has values for all columns
     
       const headers = values[0];
