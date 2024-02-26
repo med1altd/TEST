75c99a3884eb28a6239bce992fa35d94665a83c7
med1altd <139887894+med1altd@users.noreply.github.com>
2024-02-26 02:58:57 +0200

    Update updateAdRequests.js

--- api/updateAdRequests.js
+++ api/updateAdRequests.js
@@ -42,11 +42,11 @@ module.exports = async (req, res) => {
     const year = currentDate.getFullYear();
     const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Adding 1 because getMonth() returns zero-based month
     const day = String(currentDate.getDate()).padStart(2, '0');
-    const hours = String(currentDate.getHours()).padStart(2, '0') + 2;
+    const hours = String(currentDate.getHours()).padStart(2, '0');
     const minutes = String(currentDate.getMinutes()).padStart(2, '0');
     const seconds = String(currentDate.getSeconds()).padStart(2, '0');
     
-    const now = `${year}-${month}-${day}-${hours}`;
+    const now = `${year}-${month}-${day}-${hours + 2}`;
     
     // Query the Google Sheet to find today's date
     const response = await sheets.spreadsheets.values.get({
