dbe08ba48088aa872ada161efc14995c6fbf18a0
med1altd <139887894+med1altd@users.noreply.github.com>
2024-02-28 07:53:12 +0200

    Update requestAdMobRequests.js

--- api/requestAdMobRequests.js
+++ api/requestAdMobRequests.js
@@ -82,8 +82,8 @@ module.exports = async (req, res) => {
     // Calculate total requests for each ad type based on the current date
     const currentDateTotals = adIndices.map(index => currentDateValues.reduce((acc, row) => acc + parseInt(row[index] || 0), 0));
 
-    // Calculate total max requests for each ad type based on the current date
-    const maxDateTotals = adIndices.map(index => maxValues.reduce((acc, row) => acc + parseInt(row[index + 1] || 0), 0));
+    // Calculate total max requests for each ad type based on every hour of the current date
+    const totalMaxRequestsBasedOnDate = adIndices.map(index => maxValues.reduce((acc, row) => acc + parseInt(row[index + 1] || 0), 0));
 
     // Construct response object
     const responseData = adTypes.map((adType, index) => ({
@@ -91,7 +91,7 @@ module.exports = async (req, res) => {
       CurrentRequestsBasedOnHour: currentHourTotals[index],
       MaxRequestsBasedOnHour: maxHourRequests[index],
       TotalRequestsBasedOnDate: currentDateTotals[index],
-      TotalMaxRequestsBasedOnDate: maxDateTotals[index],
+      TotalMaxRequestsBasedOnDate: totalMaxRequestsBasedOnDate[index],
     }));
 
     // Send the response
